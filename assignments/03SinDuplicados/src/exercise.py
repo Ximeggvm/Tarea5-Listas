f02321b124333261a3f7135f96467762d43c4727
Ximeggvm <110927159+Ximeggvm@users.noreply.github.com>
2022-09-27 14:37:21 -0500

    Update exercise.py

--- assignments/03SinDuplicados/src/exercise.py
+++ assignments/03SinDuplicados/src/exercise.py
@@ -1,5 +1,26 @@
 def main():
     #escribe tu código abajo de esta línea
+a=0
+repetidor=0
+lista=[]
+repeticiones=[]
+contador=0
+veces=int(input())
+
+while contador<veces:
+    aa=input()
+    lista+=[aa]
+    contador=contador+1
+
+for i in lista:
+    if i in repeticiones:
+        lista.pop(a)
+    else:
+        repeticiones.append(i)
+        a=a+1
+        
+print(lista)
+
 
 if __name__=='__main__':
     main()
