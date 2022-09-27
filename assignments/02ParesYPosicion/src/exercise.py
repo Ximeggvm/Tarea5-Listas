573ca58a91611d523c1aa6ba40b3384af1554ddd
Ximeggvm <110927159+Ximeggvm@users.noreply.github.com>
2022-09-27 14:26:45 -0500

    Update exercise.py

--- assignments/02ParesYPosicion/src/exercise.py
+++ assignments/02ParesYPosicion/src/exercise.py
@@ -1,4 +1,16 @@
 def main():
+numeros=[]
+veces=int(input())
+contador=0
 
+while contador<veces:
+    aa=input()
+    numeros+=[aa]
+    contador=contador+1
+for i in range (veces):
+    valor=int(numeros[i])%2
+    if valor==0:
+        print('pos', str(i)+str(','),'valor',(numeros[i]))
+        
 if __name__=='__main__':
     main()
