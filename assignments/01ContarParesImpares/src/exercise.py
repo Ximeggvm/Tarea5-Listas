3c7cbe427a9dea02fef906aec7546b1b40db884f
Ximeggvm <110927159+Ximeggvm@users.noreply.github.com>
2022-09-27 14:13:26 -0500

    Update exercise.py

--- assignments/01ContarParesImpares/src/exercise.py
+++ assignments/01ContarParesImpares/src/exercise.py
@@ -1,6 +1,24 @@
 def main():
     #escribe tu código abajo de esta línea
-    
+valor=1
+numeros=[]
+pares=0
+impares=0
+while valor==1:
+    n=input()
+    if n=='*':
+        veces=len(numeros)
+        valor=0
+        for i in range (veces):
+            valor=int(numeros[i])%2
+            if valor==0:
+                pares=pares+1
+            if valor==1:
+                impares=impares+1
+        print('Pares='+str(pares))
+        print('Impares='+str(impares))
+        
+    numeros +=[n]
     
 
 if __name__=='__main__':
